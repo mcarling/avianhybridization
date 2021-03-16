823810c95077e5483ee19ba57e4e5ade0e0f4055
pjdougherty94 <74076243+pjdougherty94@users.noreply.github.com>
2021-03-16 00:17:38 -0600

    Update relative_abundance_modeling.R

--- relative_abundance_modeling.R
+++ relative_abundance_modeling.R
@@ -1,13 +1,14 @@
 ## This script uses the filtered and zero-filled eBird datasets prepared in the 
-## filtering script, and eBird checklist habitat and bioblim datasets prepared 
-## in the hybrid prediction script to run models that determine how different
-## variables influence checklist observation count for parental taxa (and also 
-## of hybrids if sufficient data exists).
-## It then uses the habitat and bioclim prediction surfaces to predict the relative 
-## abudnances of parental taxa across North America. Then, it uses these predicted 
+## "eBird_loading_rel_abun" script, and eBird checklist habitat and bioclim 
+## datasets prepared in the "relative_abundance_data_prep" script to run 
+## generalized additive models that that determine how different variables 
+## influence checklist observation count for parental taxa (and also of hybrids 
+## if sufficient data exists). It then uses the habitat and bioclim prediction 
+## surfaces generated in the "relative_abundance_data_prep" script to predict the 
+## relative abundances of these taxa across North America. Then, it uses these predicted 
 ## distributions to identify areas where hybridization is possible.
 
-## As with the other scripts in this project, much of this code has been taken from 
+## As with the other scripts in this folder, much of this code has been taken from 
 ## the "Best Practices for Using eBird Data" guide by Strimas-Mackey et al. (2020):
 
 ## Strimas-Mackey, M., W.M. Hochachka, V. Ruiz-Gutierrez, O.J. Robinson, E.T. Miller,
@@ -20,11 +21,11 @@
 library(raster)
 library(mgcv)
 library(sf)
-library(viridis)
-library(fields)
 library(dggridR)
 library(lubridate)
 library(tidyverse)
+
+select <- dplyr::select
 
 setwd("/Volumes/commons/CarlingLab/eBird Data/Data for looking at relative abundance")
 
@@ -32,7 +33,12 @@ setwd("/Volumes/commons/CarlingLab/eBird Data/Data for looking at relative abund
 set.seed(1)
 
 
-## loading prediction surface and required shape file ------------------------------------------
+## loading habitat and bioclim data, prediction surface, and required shape file ------------------------------------------
+# MODIS habitat covariates, elevation, and bioclim data for all checklist locations
+habitat <- read_csv("ebird_pland-elev-bioclim_location-year_final.csv") %>%
+  mutate(year = as.integer(year))
+habitat <- na.omit(habitat)
+
 # reading in the prediction surface
 pred_surface <- read_csv("pland-elev-bioclim_prediction-surface_final.csv")
 pred_surface <- na.omit(pred_surface)
@@ -47,18 +53,32 @@ r <- raster("prediction-surface_final.tif")
 dggs <- dgconstruct(spacing = 5)
 
 
-## loading and preparing records for each species --------------------------
+## to help choose which habitat and bioclim variables to indlude as predictor variables, examine collinearity among them
+names(habitat)
+# examinubg potential predictor variables
+var <- c("pland_00_water", "pland_01_evergreen_needleleaf", 
+         "pland_02_evergreen_broadleaf", "pland_03_deciduous_needleleaf", 
+         "pland_04_deciduous_broadleaf", "pland_05_mixed_forest", "pland_06_closed_shrubland", 
+         "pland_07_open_shrubland", "pland_08_woody_savanna",
+         "pland_09_savanna", "pland_10_grassland", "pland_11_wetland", 
+         "pland_12_cropland", "pland_13_urban", "pland_14_mosiac",
+         "pland_15_barren", "elevation_median", "bio10_mean_temp_warmest_quarter",
+         "bio12_annual_precip", "bio18_precip_warmest_quarter")
+var_cor <- cor(habitat2[,var])
+var_cor.df <- as.data.frame(var_cor)
+
+
+## running relative abundance models with the eBird data for each species --------------------------
 ebird_data <- c("indigo_bunting_pred_mx_half_july.csv", 
                 "lazuli_bunting_pred_mx_half_july.csv", 
                 "hybrid_bunting_pred_mx_half_july.csv")
 
-# MODIS habitat covariates, elevation, and bioclim data for all checklist locations
-habitat <- read_csv("ebird_pland-elev-bioclim_location-year_final.csv") %>%
-  mutate(year = as.integer(year))
-
-
-
+# we'll save the top model for each species
 ebird_gam <- list()
+
+# we'll also save the training dataset for each species
+ebird_split_train <- list()
+
 for(i in ebird_data){
   
   ## reading in the data and set-up
@@ -86,8 +106,7 @@ for(i in ebird_data){
       group_by(species_observed, year, week, cell) %>% 
       sample_n(size = 1) %>% 
       ungroup() %>% 
-      select(-cell, -week)}
-  else{
+      select(-cell, -week)} else{
     # NOTE: if you try to run a model for hybrids, for which there are likely extremely
     # few observtions, consider retaining all checklists with detections:
     # first divide checklists into whether or not the species was observed, then
@@ -118,18 +137,18 @@ for(i in ebird_data){
   # model. Consider what habitat types focal taxa prefer and actively avoid.
   hab.bioclim_covs <- c(
     "pland_00_water", 
-    "pland_01_evergreen_needleleaf", 
+    #"pland_01_evergreen_needleleaf", 
     #"pland_02_evergreen_broadleaf", 
     #"pland_03_deciduous_needleleaf", 
     "pland_04_deciduous_broadleaf", 
     #"pland_05_mixed_forest",
     "pland_06_closed_shrubland", 
     "pland_07_open_shrubland", 
-    "pland_08_woody_savanna",#)
+    "pland_08_woody_savanna",
     #"pland_09_savanna", 
-    "pland_10_grassland", 
+    #"pland_10_grassland", 
     #"pland_11_wetland", 
-    "pland_12_cropland", 
+    #"pland_12_cropland", 
     "pland_13_urban", 
     #"pland_14_mosiac", 
     #"pland_15_barren",
@@ -137,6 +156,9 @@ for(i in ebird_data){
     "bio12_annual_precip",
     "bio18_precip_warmest_quarter"
   )
+  ## Note, if you are unsure which predictor variables to inlcude, try model selection (e.g., AIC model selection, shrinkage, etc.)
+  ## Also, the way the script is currently set up, you have to choose the same habitat and bioclim variables for the two parental 
+  ## species. In some systems, you may wish to choose different predictor variables.
   
   ebird_split <- ebird_ss %>% 
     # select only the columns to be used in the model
@@ -154,69 +176,133 @@ for(i in ebird_data){
     split(if_else(runif(nrow(.)) <= 0.8, "train", "test"))
   map_int(ebird_split, nrow)
   
-  ebird_gam[[i]] <- ebird_split
+  ebird_split_train[[i]] <- ebird_split$train
+
+  ### running gams to model abundance for each species
+
+  ## gam parameters
+  # degrees of freedom for smoothing
+  k <- 5
+  # degrees of freedom for cyclic time of day smooth
+  k_time <- 7
+
+  # continuous predictors (excluding time, which should be cyclic)
+  continuous_covs <- ebird_split$train %>% 
+    select(-observation_count, -protocol_type, -time_observations_started) %>% 
+    names()
+
+  # create model formula for predictors
+  gam_formula_rhs <- str_glue("s({var}, k = {k})", 
+                             var = continuous_covs, k = k) %>% 
+    str_flatten(collapse = " + ") %>% 
+    str_glue(" ~ ", .,
+             " + protocol_type + ",
+            "s(time_observations_started, bs = \"cc\", k = {k})", 
+             k = k_time) %>% 
+    as.formula()
+
+  # model formula including response
+  gam_formula <- update.formula(observation_count ~ ., gam_formula_rhs)
+  gam_formula
+
+  # explicitly specify where the knots should occur for time_observations_started
+  # this ensures that the cyclic spline joins the variable at midnight
+  # this won't happen by default if there are no data near midnight
+  time_knots <- list(time_observations_started = seq(0, 24, length.out = k_time))
+
+  ## per Strimas-Mackey et al. (2020), eBird checklists are almost always heavily zero-inflated 
+  ## (there are far more checklists that don't report your focal taxa than ones that do). 
+  ## Therefore, we'll run our gam with three error distributions friendly to zero-inflated data,
+  ## and then assess which one performed the best.
+
+  ## Note: it can take a really, really long time to run these models, especially if you're 
+  ## including observations from all of North America. I'd recommend running this step on a
+  # higher performance computing cluster if you have one available. To save time, run only 
+  # the negative binomial models.
+
+  # zero-inflated poisson
+  m_ziplss <- gam(list(gam_formula,      # count model
+                      gam_formula[-2]), # presence model
+                 data = ebird_split$train, 
+                  family = "ziplss", 
+                  knots = time_knots)
+
+  # negative binomial
+  m_nb <- gam(gam_formula,
+             data = ebird_split$train, 
+             family = "nb",
+             knots = time_knots)
+
+  # tweedie distribution
+  m_tw <- gam(gam_formula,
+             data =  ebird_split$train, 
+             family = "tw",
+             knots = time_knots)
+
+  ## to choose which model fits the data the best, and should be used going forward,
+
+  obs_count <- select(ebird_split$test, obs = observation_count)
+
+  # presence probability is on the complimentary log-log scale
+  # we can get the inverse link function with
+  inv_link <- binomial(link = "cloglog")$linkinv
+  # combine ziplss presence and count predictions
+  m_ziplss_pred <- predict(m_ziplss, ebird_split$test, type = "link") %>% 
+    as.data.frame() %>%
+    transmute(family = "Zero-inflated Poisson", model = "m_ziplss",
+              pred = inv_link(V2) * exp(V1)) %>% 
+    bind_cols(obs_count)
+
+  m_nb_pred <- predict(m_nb, ebird_split$test, type = "response") %>% 
+    tibble(family = "Negative Binomial", model = "m_nb", pred = .) %>% 
+    bind_cols(obs_count)
+
+  m_tw_pred <- predict(m_tw, ebird_split$test, type = "response") %>% 
+    tibble(family = "Tweedie", model = "m_tw", pred = .) %>% 
+    bind_cols(obs_count)
+
+
+  # combine predictions from all three models
+  test_pred <- bind_rows(m_ziplss_pred, m_nb_pred, m_tw_pred)
+
+  ## now evaluating the model based on Spearman's rank correlation
+  # spearman’s rank correlation
+  test_pred %>% 
+    group_by(family, model) %>% 
+    summarise(rank_cor = cor.test(obs, pred, 
+                                 method = "spearman", 
+                                  exact = FALSE)$estimate) %>% 
+    ungroup()
+
+  # extracting the model with the highest Spearman's rank correlation
+  pred_model <- test_pred_cor[which.max(test_pred_cor$rank_cor),]$model
+  
+  ebird_gam[[i]] <- get(pred_model)
 }
 
+## model validation for the top model for each species
+gam.check(ebird_gam[[1]]) # for Indigos
+gam.check(ebird_gam[[2]]) # for Lazulis
+gam.check(ebird_gam[[3]]) # for hybrids
 
-# running gams to model abundance for each species ------------------------
-
-## gam parameters
-# degrees of freedom for smoothing
-k <- 5
-# degrees of freedom for cyclic time of day smooth
-k_time <- 7
-
-# continuous predictors (excluding time, which will be treated as cyclic)
-continuous_covs <- ebird_gam[[1]]$train %>% 
-  select(-observation_count, -protocol_type, -time_observations_started) %>% 
-  names()
-
-# create model formula for predictors
-gam_formula_rhs <- str_glue("s({var}, k = {k})", 
-                            var = continuous_covs, k = k) %>% 
-  str_flatten(collapse = " + ") %>% 
-  str_glue(" ~ ", .,
-           " + protocol_type + ",
-           "s(time_observations_started, bs = \"cc\", k = {k})", 
-           k = k_time) %>% 
-  as.formula()
-
-# model formula including response
-gam_formula <- update.formula(observation_count ~ ., gam_formula_rhs)
-gam_formula
-
-# trying to do the model with different error distributions
-# explicitly specify where the knots should occur for time_observations_started
-# this ensures that the cyclic spline joins the variable at midnight
-# this won't happen by default if there are no data near midnight
-time_knots <- list(time_observations_started = seq(0, 24, length.out = k_time))
-
-# negative binomial
-m_nb <- gam(gam_formula,
-            data = ebird_gam[[i]]$train, 
-            family = "nb",
-            knots = time_knots)
-
-
-
-### examining covariate effects
+## examining covariate effects for each species seperately
 # ggplot function
 plot_gam <- function(m, title = NULL, ziplss = c("presence", "abundance")) {
-  # capture plot
-  tmp <- tempfile()
-  png(tmp)
-  p <- plot(m, pages = 1)
-  dev.off()
-  unlink(tmp)
+# capture plot
+ tmp <- tempfile()
+ png(tmp)
+ p <- plot(m, pages = 1)
+ dev.off()
+ unlink(tmp)
   
   # drop addition models in ziplss
-  if (m$family$family == "ziplss") {
-    is_presence <- map_lgl(p, ~ str_detect(.$ylab, "^s\\.1"))
-    if (ziplss == "presence") {
-      p <- p[is_presence]  
-    } else {
-      p <- p[!is_presence]
-    }
+ if (m$family$family == "ziplss") {
+  is_presence <- map_lgl(p, ~ str_detect(.$ylab, "^s\\.1"))
+  if (ziplss == "presence") {
+    p <- p[is_presence]  
+  } else {
+    p <- p[!is_presence]
+  }
   }
   
   # extract data
@@ -237,78 +323,74 @@ plot_gam <- function(m, title = NULL, ziplss = c("presence", "abundance")) {
   invisible(p_df)
 }
 
-plot_gam(m_nb, title = "Negative Binomial GAM")
+plot_gam(ebird_gam[[1]], title = "Indigo Bunting")
+plot_gam(ebird_gam[[2]], title = "Lazuli Bunting")
+plot_gam(ebird_gam[[3]], title = "Hybrid Indigo x Lazuli Bunting")
 
-indigo_m_nb <- m_nb
-pred_model <- m_nb
 
-seq_tod <- seq(0, 24, length.out = 300)
-tod_df <- ebird_split$train %>% 
-  # find average pland habitat covariates
-  select(starts_with("pland"), starts_with("bio"), latitude, longitude, elevation_median) %>% 
-  summarize_all(mean, na.rm = TRUE) %>% 
-  ungroup() %>% 
-  # use standard checklist
-  mutate(day_of_year = yday(ymd(str_glue("{max_lc_year}-06-15"))),
-         duration_minutes = 60,
-         effort_distance_km = 1,
-         number_observers = 1,
-         protocol_type = "Traveling") %>% 
-  cbind(time_observations_started = seq_tod)
+## predicting relative abundance across a focal area based on the top model output --------------------------
 
-# predict at different start times
-pred_tod <- predict(pred_model, newdata = tod_df, 
+for(i in ebird_gam)
+
+# we'll save the top model for each species
+ebird_gam <- list()
+
+# we'll also save the training dataset for each species
+ebird_split_train <- list()
+
+for(i in ebird_data){
+ seq_tod <- seq(0, 24, length.out = 300)
+ tod_df <- ebird_split$train %>% 
+    # find average pland habitat covariates
+    select(starts_with("pland"), starts_with("bio"), latitude, longitude, elevation_median) %>% 
+    summarize_all(mean, na.rm = TRUE) %>% 
+    ungroup() %>% 
+    # use standard checklist
+    mutate(day_of_year = yday(ymd(str_glue("{max_lc_year}-06-15"))),
+          duration_minutes = 60,
+          effort_distance_km = 1,
+          number_observers = 1,
+          protocol_type = "Traveling") %>% 
+    cbind(time_observations_started = seq_tod)
+
+  # predict at different start times
+  pred_tod <- predict(ebird_gam[[i]], newdata = tod_df, 
                     type = "link", 
                     se.fit = TRUE) %>% 
-  as_tibble() %>% 
-  # calculate backtransformed confidence limits
-  transmute(time_observations_started = seq_tod,
-            pred = pred_model$family$linkinv(fit),
-            pred_lcl = pred_model$family$linkinv(fit - 1.96 * se.fit),
-            pred_ucl = pred_model$family$linkinv(fit + 1.96 * se.fit))
+    as_tibble() %>% 
+    # calculate backtransformed confidence limits
+    transmute(time_observations_started = seq_tod,
+              pred = pred_model$family$linkinv(fit),
+              pred_lcl = pred_model$family$linkinv(fit - 1.96 * se.fit),
+              pred_ucl = pred_model$family$linkinv(fit + 1.96 * se.fit))
 
-# find optimal time of day
-t_peak <- pred_tod$time_observations_started[which.max(pred_tod$pred_lcl)]
+  # find optimal time of day
+  t_peak <- pred_tod$time_observations_started[which.max(pred_tod$pred_lcl)]
 
-# plot the partial dependence plot
-ggplot(pred_tod) +
-  aes(x = time_observations_started, y = pred,
-      ymin = pred_lcl, ymax = pred_ucl) +
-  geom_ribbon(fill = "grey80", alpha = 0.5) +
-  geom_line() +
-  geom_vline(xintercept = t_peak, color = "blue", linetype = "dashed") +
-  labs(x = "Hours since midnight",
-       y = "Predicted relative abundance",
-       title = "Effect of observation start time on Indigo Bunting reporting",
-       subtitle = "Peak detectability shown as dashed blue line")
+  # add effort covariates to prediction surface
+  pred_surface_eff <- pred_surface %>% 
+    mutate(day_of_year = yday(ymd(str_glue("{max_lc_year}-06-15"))),
+          time_observations_started = t_peak,
+          duration_minutes = 60,
+          effort_distance_km = 1,
+          number_observers = 1,
+          protocol_type = "Traveling")
 
-# add effort covariates to prediction surface
-pred_surface_eff <- pred_surface %>% 
-  mutate(day_of_year = yday(ymd(str_glue("{max_lc_year}-06-15"))),
-         time_observations_started = t_peak,
-         duration_minutes = 60,
-         effort_distance_km = 1,
-         number_observers = 1,
-         protocol_type = "Traveling")
-
-# predict
-#### maybe need to unscale latitude, longitude, and elevation before fitting model to prediction surface!!!
-##### if you do try to scale latitude, longitude, and elevation, maybe try to scale those variables 
-##### in the prediction dataframe, then predict, and then unscale in the new dataframe for mapping
-indigo_pred <- predict(pred_model, newdata = pred_surface_eff, # for Indigos
-#lazuli_pred <- predict(pred_model, newdata = pred_surface_eff, # for Lazulis
-                       #hybrid_pred <- predict(pred_model, newdata = pred_surface_eff, # for hybrids                       
+  ### predict
+  ebird_pred <- predict(ebird_gam[[i]], newdata = pred_surface_eff,                       
                        type = "link", 
                        se.fit = TRUE) %>% 
   as_tibble() %>% 
   # calculate confidence limits and back transform
-  transmute(abd = pred_model$family$linkinv(fit),
-            abd_se = pred_model$family$linkinv(se.fit),
-            abd_lcl = pred_model$family$linkinv(fit - 1.96 * se.fit),
-            abd_ucl = pred_model$family$linkinv(fit + 1.96 * se.fit)) %>%
+  transmute(abd = ebird_gam[[i]]$family$linkinv(fit),
+            abd_se = ebird_gam[[i]]$family$linkinv(se.fit),
+            abd_lcl = ebird_gam[[i]]$family$linkinv(fit - 1.96 * se.fit),
+            abd_ucl = ebird_gam[[i]]$family$linkinv(fit + 1.96 * se.fit)) %>%
   # add to prediction surface
   bind_cols(pred_surface_eff, .) %>% 
   select(latitude, longitude, abd, abd_se, abd_lcl, abd_ucl)
+  
+
 
 write.csv(indigo_pred, "indigo_pred_nb_final_final.csv", na = "", row.names=FALSE) # for Indigos
 
@@ -316,6 +398,7 @@ write.csv(indigo_pred, "indigo_pred_nb_final_final.csv", na = "", row.names=FALS
 ## for model validation, look at distribution of residuals
 
 ## also looking at influential observations
+
 
 
 
@@ -360,9 +443,6 @@ plot(ne_land_modis)
 
 
 ne_land_trim2 <- ne_land_trim %>% 
-  st_transform(crs = map_proj) %>% 
-  st_geometry()
-bcr <- read_sf("data_eBird_best_practices/gis-data.gpkg", "bcr") %>% 
   st_transform(crs = map_proj) %>% 
   st_geometry()
 ne_country_lines <- read_sf("data_eBird_best_practices/gis-data.gpkg", "ne_country_lines") %>% 
